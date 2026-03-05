ab5686cd9da6590808a8bbd8d4eb8350a85588e8
Peniyalpradeesh <peniyalpradeepan@gmail.com>
2026-03-05 19:28:26 +0000

    Implemented trajectory time calculations using the given joint velocity limits.
    Set up a debug output in main to ensure the program up to this point for trajectory planning worked as expected.

--- robot/TrajectoryPlanner.cpp
+++ robot/TrajectoryPlanner.cpp
@@ -1,8 +1,13 @@
 #include "TrajectoryPlanner.h"
 #include "Validation.h"
-
 #include "Collision.h"
+#include "Constants.h"
+
 #include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace Constants;
 
 
 std::vector<JointState> planTrajectory(
@@ -17,6 +22,27 @@ std::vector<JointState> planTrajectory(
         std::cout << "Unreachable\n";
         return trajectory;
     }
+    
+    //calc the distance difference between target and origin
+    double diffU1 = target.U1 - origin.U1;
+    double diffU2 = target.U2 - origin.U2;
+    double diffR1 = target.R1 - origin.R1;
+    double diffR2 = target.R2 - origin.R2;
+    double diffP1 = target.P1 - origin.P1;
+
+    //calc travel time for each joint using the max speed (time = dist/speed)
+    //abs used since time must be positive
+    double timeU1 = std::abs(diffU1) / 30.0;
+    double timeU2 = std::abs(diffU2) / 30.0;
+    double timeR1 = std::abs(diffR1) / 45.0;
+    double timeR2 = std::abs(diffR2) / 45.0;
+    double timeP1 = std::abs(diffP1) / 10.0;
+
+    //used to find the maximum time taken
+    double maxTime = std::max({ timeU1, timeU2, timeR1, timeR2, timeP1});
+
+    std::cout << "Trajectory time: " << maxTime << "seconds\n";
+    
 
     trajectory.push_back(origin); //adds origin config to trajectory
     
--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,29 @@
 #include "ForwardKinematics.h"
 #include "Validation.h"
 #include "Collision.h"
+#include "TrajectoryPlanner.h"
 
 
 int main(){
     JointState joints;
 
     //used to verify forward kinematics computation
-    joints.U1 = -45.0;
-    joints.U2 = 40.0;
-    joints.R1 = 140.0;
-    joints.R2 = 0.0;
-    joints.P1 = 50.0;
+    joints.U1 = 45.0;
+    joints.U2 = -90.0;
+    joints.R1 = -90.0;
+    joints.R2 = -90.0;
+    joints.P1 = 60.0;
 
     // check if joint values are within allowed limits
     if(!isWithinLimits(joints)){
         std::cout << "||Unreachable||\n";
-        return 0;
+       // return 0;
     }
 
     //checks if configuration causes a collision
     if(collision(joints)){
         std::cout << "||Collision||\n";
-        return 0;
+      //  return 0;
     }
     
 
@@ -37,6 +38,16 @@ int main(){
     std::cout << "Y: " << pos.y << "mm" << "\n";
     std::cout << "Z: " << pos.z << "mm" << "\n";
 
+
+
+    JointState origin = {0, 0, 0 , 0, 0};
+    JointState target = {45, -90, -90, -90, 50};
+
+    auto trajectory = planTrajectory(origin, target);
+
+    std::cout << "Trajectory size: " << trajectory.size() << "\n";
+
     return 0;
+
 
 }
